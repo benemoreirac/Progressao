840fcfc48cf2b019c3766b707b90cb5b2e332e4b
Ebenezer Moreira <ebenezer.moreira90@gmail.com>
2021-08-03 17:14:47 -0300

    Commit até videoAula 04

--- paginas/urls.py
+++ paginas/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from django.urls.resolvers import URLPattern
 
-from .views import IndexView
+from .views import IndexView, SobreView
 
 urlpatterns = [
     path('', IndexView.as_view(), name='inicio'),
+    path('sobre/', SobreView.as_view(), name='sobre'),
 ]
