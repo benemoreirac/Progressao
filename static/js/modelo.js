33773893b7eb6dc8b9adc597928ccded540f1a9d
Ebenezer Moreira <ebenezer.moreira90@gmail.com>
2021-08-12 16:04:29 -0300

    Ajuste Nav(header) e Criação do Footer

--- static/js/modelo.js
+++ static/js/modelo.js
@@ -1,8 +1,19 @@
 const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(){
+function toggleMenu(event){
+    if (event.type ==='touchstart') event.preventDefault()
     const nav = document.getElementById('nav-list');
-    nav.classList.toggle('active')
+    nav.classList.toggle('active');
+
+    const active = nav.classList.contains('active');
+    event.currentTarget.setAttribute('aria-expanded', active);
+
+    if (active){
+        event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    } else{
+        event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
 }
 
 btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
