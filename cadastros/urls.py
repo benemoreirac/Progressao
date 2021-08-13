98484667bbd967244939e6986ec764c19f986a58
Ebenezer Moreira <ebenezer.moreira90@gmail.com>
2021-08-13 18:31:23 -0300

    Atualização até a aula 08, Classes no painel Adm

---
+++ cadastros/urls.py
@@ -0,0 +1,5 @@
+from django.urls import path
+
+urlpatterns = [
+    #path('endereco', MinhaView.as_view(), name='name'),
+]
--- paginas/urls.py
+++ paginas/urls.py
@@ -4,6 +4,6 @@ from django.urls.resolvers import URLPattern
 from .views import IndexView, SobreView
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='inicio'),
+    path('home', IndexView.as_view(), name='home'),
     path('sobre/', SobreView.as_view(), name='sobre'),
 ]
