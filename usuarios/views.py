5f3ff7de501c3811d92e5ff108fb6f1d863419ba
Ebenezer Moreira <ebenezer.moreira90@gmail.com>
2021-08-30 16:50:43 -0300

    Adcionar usuario em um grupo em momento de Registro

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,13 +1,21 @@
 from typing import Any, Dict
 from django.views.generic.edit import CreateView
-from django.contrib.auth.models import User 
+from django.contrib.auth.models import User, Group 
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
+from django.shortcuts import get_object_or_404
 
 class UsuarioCreate(CreateView):
     template_name = "cadastros/form.html"
     form_class = UsuarioForm
     success_url = reverse_lazy('login')
+
+    def form_valid(self, form):
+        grupo = get_object_or_404(Group, name="Docente")
+        url = super().form_valid(form)
+        self.object.groups.add(grupo)
+        self.object.save()
+        return url
     
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
