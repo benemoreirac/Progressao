38d6e1b93189a4de4cb15e3532b1abe75b036e0d
Ebenezer Moreira <ebenezer.moreira90@gmail.com>
2021-08-30 22:47:36 -0300

    Criação de classe Perfil no usuario Modulo e editar/exebir perfis do usuario!

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,9 +1,10 @@
 from typing import Any, Dict
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
 from django.contrib.auth.models import User, Group 
 from .forms import UsuarioForm
 from django.urls import reverse_lazy
 from django.shortcuts import get_object_or_404
+from .models import Perfil
 
 class UsuarioCreate(CreateView):
     template_name = "cadastros/form.html"
@@ -15,10 +16,28 @@ class UsuarioCreate(CreateView):
         url = super().form_valid(form)
         self.object.groups.add(grupo)
         self.object.save()
+        Perfil.objects.create(usuario = self.object)
         return url
     
     def get_context_data(self, *args, **kwargs):
         context = super().get_context_data(*args, **kwargs)
         context['titulo'] = "Registro de Novo Usuário"
         context['botao'] = "Cadastrar"
+        return context
+
+
+class PerfilUpdate(UpdateView):
+    template_name = 'cadastros/form.html'
+    model = Perfil
+    fields = ['nome_completo','cpf','telefone']
+    success_url = 'index'
+
+    def get_object(self, queryset=None):
+        self.object = get_object_or_404(Perfil, usuario=self.request.user)
+        return self.object
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        context['titulo'] = "Meus dados Pessoais"
+        context['botao'] = "Atualizar"
         return context
